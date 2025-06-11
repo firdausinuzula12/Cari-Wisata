a3f387aff5a22039aaadb91e62f16576ec51585b
firdausinuzula12 <firdausinuzula4578@gmail.com>
2025-06-11 13:53:17 +0700

    Delete db_barang directory

--- db_barang/proses_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include 'koneksi.php'; 
-    $id = $_GET['id']; 
- 
-    $query = "DELETE FROM tb_barang WHERE id='$id'"; 
-    mysqli_query($koneksi, $query); 
- 
-    header('Location: tampilan.php'); //pindah kehalaman tampilan.php
-?> 
